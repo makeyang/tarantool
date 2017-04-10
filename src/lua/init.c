210afd6df73ad318b80e50e12d9ba373a4415100
bigbes <bigbes@gmail.com>
2017-04-10 17:23:23 +0300

    Adding string.ljust(), string.rjust() and string.center()
    * `string.ljust(inp, width[, char])`  - returns left-justified string
      filled with charachter 'char' (' ' by default).
    * `string.rjust(inp, width[, char])`  - returns right-justified string
      filled with charachter 'char' (' ' by default)
    * `string.center(inp, width[, char])` - returns centered string
      filled with charachter 'char' (' ' by default)
    
    closes gh-2214

--- src/lua/init.c
+++ src/lua/init.c
@@ -102,6 +102,7 @@ extern char strict_lua[],
 	title_lua[],
 	env_lua[],
 	trigger_lua[],
+	ext_string_lua[],
 	p_lua[], /* LuaJIT 2.1 profiler */
 	zone_lua[] /* LuaJIT 2.1 profiler */;
 
@@ -128,6 +129,7 @@ static const char *lua_modules[] = {
 	"tap", tap_lua,
 	"help.en_US", help_en_US_lua,
 	"help", help_lua,
+	"ext_string", ext_string_lua,
 	"internal.argparse", argparse_lua,
 	"internal.trigger", trigger_lua,
 	/* jit.* library */
